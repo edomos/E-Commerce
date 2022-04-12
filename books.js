d694eafe31838e9ea2fd9f9e96e4c10d51eea3d2
edomos <alf.ramoz@gmail.com>
2022-04-12 00:03:19 -0700

    create javascript function to sort books by price/rating

--- books.js
+++ books.js
@@ -1,3 +1,50 @@
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books")
+  const books = getBooks();
+
+  if (filter === 'LOW_TO_HIGH') {
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+  } else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  } else if (filter === 'RATING') {
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
+  
+
+  const booksHtml = books.map(book => {
+    return `<div class="book">
+<figure class="book__img--wrapper">
+  <img class="book__img" src="${book.url}" alt="">
+</figure>
+<div class="book__title">
+  ${book.title}
+</div>
+<div class="book__ratings">
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star-half-alt"></i>
+</div>
+<div class="book__price">
+  <span>$${book.originalPrice.toFixed(2)}</span>
+</div>
+</div>`
+  })
+    .join("")
+  booksWrapper.innerHTML = booksHtml;
+  // booksWrapper.innerHTML = 
+}
+
+function filterBooks(event) {
+  renderBooks(event.target.value)
+}
+
+setTimeout(() => {
+  renderBooks();
+});
+
 
 
 // FAKE DATA
@@ -6,7 +53,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
