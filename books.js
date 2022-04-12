3d18452fa27b582b83fe14944c9cdf242f0dcd76
edomos <alf.ramoz@gmail.com>
2022-04-12 00:14:41 -0700

    create js function to sort books by rating

--- books.js
+++ books.js
@@ -10,8 +10,6 @@ function renderBooks(filter) {
     books.sort((a, b) => b.rating - a.rating);
   }
 
-  
-
   const booksHtml = books.map(book => {
     return `<div class="book">
 <figure class="book__img--wrapper">
@@ -21,11 +19,7 @@ function renderBooks(filter) {
   ${book.title}
 </div>
 <div class="book__ratings">
-  <i class="fas fa-star"></i>
-  <i class="fas fa-star"></i>
-  <i class="fas fa-star"></i>
-  <i class="fas fa-star"></i>
-  <i class="fas fa-star-half-alt"></i>
+ ${ratingsHTML(book.rating)}
 </div>
 <div class="book__price">
   <span>$${book.originalPrice.toFixed(2)}</span>
@@ -35,6 +29,17 @@ function renderBooks(filter) {
     .join("")
   booksWrapper.innerHTML = booksHtml;
   // booksWrapper.innerHTML = 
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = '';
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += `<i class="fas fa-star"></i>\n`
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`
+  }
+  return ratingHTML;
 }
 
 function filterBooks(event) {
